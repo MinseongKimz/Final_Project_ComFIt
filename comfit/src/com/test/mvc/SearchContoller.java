afd300ba329163a18c2f9a530667b4a359b4cbd7
MinseongKimz <minseongkimc@gmail.com>
2022-06-10 16:21:01 +0900

    물품검색 실제 구현

---
+++ comfit/src/com/test/mvc/SearchContoller.java
@@ -0,0 +1,24 @@
+package com.test.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SearchContoller
+{
+	
+	@RequestMapping(value = "/search.action", method = RequestMethod.POST)
+	public String search(Model model, @RequestParam("name") String name)
+	{
+		String result= null;
+		
+		result = NaversearchAPI.search(name);
+		
+		model.addAttribute("result", result);
+		
+		return "user_search_product.jsp";
+	}
+}

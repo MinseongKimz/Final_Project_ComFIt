8393fbc3f6f16500f2dd0ef252f93135c335c7e9
MinseongKimz <minseongkimc@gmail.com>
2022-06-12 18:15:33 +0900

    2022-06-12 정리 상태

--- comfit/src/com/test/mybatis/IAdmin.java
+++ comfit/src/com/test/mybatis/IAdmin.java
@@ -8,7 +8,7 @@ public interface IAdmin
 	public ArrayList<userDTO> adminUserList();
 	
 	// 관리자모드 카테고리 리스트 출력 메소드
-	public ArrayList<categoryDTO> categoryList();
+	public ArrayList<CategoryDTO> categoryList();
 	
 	// 관리자모드 카테고리 추가 메소드
 	public int categoryInsert(String category_name);

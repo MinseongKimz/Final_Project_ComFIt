cb2bcf128bf1e6efa234184cf70927c3eca61bc1
seonha <cseon230@gmail.com>
2022-06-13 18:31:20 +0900

    FAQ페이지 추가,수정 기능

--- comfit/src/com/test/mybatis/IAdmin.java
+++ comfit/src/com/test/mybatis/IAdmin.java
@@ -16,11 +16,23 @@ public interface IAdmin
 	// 관리자모드 카테고리 추가 메소드
 	public int categoryInsert(String category_name);
 	
+	// 관리자모드 카테고리 삭제 메소드
 	public int categoryDelete(int pd_category_id);
 	
 	// 관리자모드 탈퇴한 회원 리스트 출력 메소드
 	public ArrayList<useroutDTO> adminUseroutList();
 	
-
+	// 관리자모드 FAQ 리스트 출력 메소드
+	public ArrayList<FaqDTO> faqList();
+	
+	// 관리자모드 FAQ 추가 메소드
+	public int faqInsert(FaqDTO f);
+	
+	// 관리자모드 FAQ 수정 폼 접속 메소드
+	public FaqDTO faqModifyForm(String faq_id);
+	
+	// 관리자모드 FAQ 수정 메소드
+	public int faqModify(FaqDTO dto);
+	
 }
 
